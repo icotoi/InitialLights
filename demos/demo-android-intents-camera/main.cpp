7f00f5cfc4019910b95f47fc8aed32fb8a083b52
Ioan Călin <iborco@gmail.com>
2019-05-30 09:51:38 +0300

    fix camera reference

--- demos/demo-android-intents-camera/main.cpp
+++ demos/demo-android-intents-camera/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty(QStringLiteral("camera"), QVariant::fromValue(&engine));
+    engine.rootContext()->setContextProperty(QStringLiteral("camera"), QVariant::fromValue(&platformCamera));
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
